afdbc224b0ee575fc63d51af18e0cc807a8388a8
vinaygautam93 <vinaygautam935680@gmail.com>
2025-02-24 01:04:50 +0530

    [Vinay] Added User.java for personalized greeting

---
+++ src/main/java/com/example/greetingapp/model/User.java
@@ -0,0 +1,31 @@
+package com.example.greetingapp.model;
+
+// This class represents the user details for personalized greeting
+public class User {
+
+    // First name of the user
+    private String firstName;
+
+    // Last name of the user
+    private String lastName;
+
+    // Getter for first name
+    public String getFirstName() {
+        return firstName;
+    }
+
+    // Setter for first name
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    // Getter for last name
+    public String getLastName() {
+        return lastName;
+    }
+
+    // Setter for last name
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
