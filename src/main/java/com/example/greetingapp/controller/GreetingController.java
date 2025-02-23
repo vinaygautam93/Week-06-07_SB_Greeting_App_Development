534fe9224d4578b2d57737577029775eacac026b
vinaygautam93 <vinaygautam935680@gmail.com>
2025-02-24 00:49:19 +0530

    [Vinay] Added UC3: Personalized Greeting

--- src/main/java/com/example/greetingapp/controller/GreetingController.java
+++ src/main/java/com/example/greetingapp/controller/GreetingController.java
@@ -1,19 +1,35 @@
 package com.example.greetingapp.controller;
 
+import com.example.greetingapp.model.User;
+import com.example.greetingapp.service.GreetingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-// This class handles REST API endpoints for greeting messages
-@RestController  // Marks this class as a REST controller
-@RequestMapping("/greeting")  // Base path for all endpoints in this controller
+// Controller class for greeting endpoints
+@RestController
+@RequestMapping("/greeting")
 public class GreetingController {
 
-    // Endpoint to return a simple hello message
+    // Injecting service layer
+    @Autowired
+    private GreetingService greetingService;
+
+    // Endpoint to get a simple greeting
     @GetMapping("/hello")
     public String getHelloMessage() {
-        // Returning a static greeting message
-        return "Hello from BridgeLabz!";
+        return greetingService.getGreetingMessage();
+    }
+
+    // Endpoint to get a personalized greeting using query parameters
+    @GetMapping("/personalized")
+    public String getPersonalizedGreeting(@RequestParam(required = false) String firstName,
+                                          @RequestParam(required = false) String lastName) {
+        // Creating User object
+        User user = new User();
+        user.setFirstName(firstName);
+        user.setLastName(lastName);
+
+        // Calling service to get greeting
+        return greetingService.getPersonalizedGreeting(user);
     }
 }
